c37dd73a56d103f7c6abb23ab49d84c1d2b50c98
Leonardo Razovic <lrazovic@iMac-di-Leonardo.local>
2018-03-07 22:09:51 +0100

    iOS: First Firebase setup

--- iOS App/Pods/Target Support Files/Pods-Beerer/Pods-Beerer-frameworks.sh
+++ iOS App/Pods/Target Support Files/Pods-Beerer/Pods-Beerer-frameworks.sh
@@ -134,11 +134,15 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pages/Pages.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/pop/pop.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pages/Pages.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/pop/pop.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
