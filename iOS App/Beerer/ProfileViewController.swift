2e393fb68646103c7bb82688a4162e4ad3a51388
Leonardo <leorazzo@gmail.com>
2018-03-17 11:41:06 +0100

    iOS: Profile View

--- iOS App/Beerer/ProfileViewController.swift
+++ iOS App/Beerer/ProfileViewController.swift
@@ -13,6 +13,7 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     
     }
 
