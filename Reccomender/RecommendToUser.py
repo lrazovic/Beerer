3513143439d163894c2bacc7d5ec0f49f8f14932
Leonardo Razovic <leorazzo@gmail.com>
2018-04-17 18:08:03 +0200

    Recommender: Update on Recommends given userId

--- Reccomender/RecommendToUser.py
+++ Reccomender/RecommendToUser.py
@@ -8,52 +8,54 @@ import argparse
 import logging
 import os
 import time
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 import csv
 from scipy.sparse import coo_matrix
 
 from implicit.als import AlternatingLeastSquares
+from implicit.bpr import BayesianPersonalizedRanking
 from implicit.nearest_neighbours import (BM25Recommender, CosineRecommender,
                                          TFIDFRecommender, bm25_weight)
 
-def choiceBeers(userId, path):
-    """ Reads in the dataset """
-    recommends = open(path + "testRecommendation.csv", "r")
-    allFile = csv.reader(recommends, delimiter=",")
-    idBeers = [] # list of beers-ID
-    count = 0
-    for row in allFile:
-        if(count != 0):
-            if(row[0] == userId and row[2] == '1'):
-                idBeers.append(row[1])
-        count+=1
-    recommends.close()
 
+def choiceBeers(userId, path):
+
+    # Get beerID for given userId
+    recommends = pd.read_csv(path + "testRecommendation.csv")
+    # print((recommends["USR"]) == userId) 
+    # data_filtered = recommends[userId]
+    idBeers = []  # list of beers-ID
+
+
+    # Use Panda
     names = open(path + "testBeer.csv", "r")
     allFile2 = csv.reader(names, delimiter=",")
-    namedBeers = [] # list of beers-names
+    namedBeers = []  # list of beers-names
     count = 0
+
     for row in allFile2:
         if(count != 0):
             if(row[0] in idBeers):
                 namedBeers.append(row[1])
-        count+=1
+        count += 1
     names.close()
 
+    # Use Panda
     similars = open(path+"result.csv", "r")
     allFile3 = csv.reader(similars, delimiter=",")
     result = []
+
     for row in allFile3:
         for i in range(0, len(namedBeers)):
-            if(row[0] == namedBeers[i] and float(row[2])>=0.6):
+            if(row[0] == namedBeers[i] and float(row[2]) >= 0.6):
                 result.append(row[1])
 
     result = list(set(result))
     return result
 
 
-
 if __name__ == "__main__":
-    path = "/home/ale96/Documents/beerer/Beerer/.idea/src/dataset/"
+    # Change Path
+    path = "/Users/lrazovic/Projects/Beerer/Reccomender/dataset/"
     print(choiceBeers("4", path))
